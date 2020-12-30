654914b8eeb1448a29ca8c55f153d5f7bf8ae8e9
Jayson1001 <64107977+Jayson1001@users.noreply.github.com>
2020-12-30 15:54:01 -0500

    initial setup

---
+++ src/components/ServicesCard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="body"></div>
+</template>
+
+<script>
+export default {
+  name: "ServicesCard",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.body {
+  width: 100%;
+}
+</style>
---
+++ src/views/Services.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="services">
+    <ServicesCard />
+    <h1>This is the services page</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import ServicesCard from "@/components/ServicesCard.vue";
+
+export default {
+  name: "Services",
+  components: {
+    ServicesCard
+  }
+};
+</script>
